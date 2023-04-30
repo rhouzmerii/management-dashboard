b722da92cee7747fcf851f7a73e239c1f49922a8
rose-jpg <okhuomonajayi54@gmail.com>
2023-04-30 09:31:34 +0100

    commit

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -6,14 +6,17 @@ import { IconContext } from "react-icons/lib";
 function Orders() {
   return (
     <motion.div>
-      <header className="h-[70px] w-[100%] bg-[#f7f7f7] flex justify-between items-center px-6">
-        <h4 className="font-bold text-[#62AB4D] text-lg">ORDERS</h4>
+      <header className="h-[55px] sm:h-[70px] w-[100%] bg-[#f7f7f7] flex justify-between items-center px-3 sm:px-6">
+        <h4 className="font-bold text-[#62AB4D] text-md sm:text-lg">ORDERS</h4>
 
-        <IconContext.Provider value={{ color: "#62AB4D", size: "30px" }}>
-         <div className="flex gap-2 items-center">
-     <div className="h-[45px] w-[45px] bg-white rounded-full flex justify-center items-center">      <h6 className="font-bold text-[#62AB4D] text-lg">RV</h6></div>
-         <VscBellDot />
-         </div>
+        <IconContext.Provider value={{ color: "#62AB4D", size: "25px" }}>
+          <div className="flex gap-2 items-center">
+            <div className="sm:h-[45px] sm:w-[45px] w-[40px] h-[40px] bg-white rounded-full flex justify-center items-center">
+              {" "}
+              <h6 className="font-bold text-[#62AB4D] text-md sm:text-lg">RV</h6>
+            </div>
+            <VscBellDot />
+          </div>
         </IconContext.Provider>
       </header>
     </motion.div>
